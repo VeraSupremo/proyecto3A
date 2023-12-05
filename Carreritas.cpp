f68b4a022e24b46cca158defffe09628cedcb7b2
VeraSupremo <martiniverinirx@gmail.com>
2023-12-05 12:26:24 -0300

    1
    se crearon los nombres de las funciones y se creo el menu para incicializar las funciones de l acarrera basadisimo

---
+++ Carreritas.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <cstdlib>
+#include <windows.h>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+//cuadraticos
+void insertion_sort(){
+	
+	
+}
+void bubble_sort(){
+	
+}
+void selection_sort(){
+	
+}
+void shell_sort(){
+	
+}
+
+
+//logaritmicos
+void merge_sort(){
+	
+}
+void quick_sort(){
+	
+}
+void heap_sort(){
+	
+}
+
+void formacion1(){ //generar los numeros de forma ordenada
+	
+	
+	
+}
+
+//esta parte se realizara mediante una cola para poder tener un mayor rango en la caacidad de gregar los numeros
+void formacion2(){ //generar los numeros de forma desordenada     
+	
+}
+void formacion3(){ //generar los numeros a la inversa de la primera 
+	
+}
+void formacion4(){ // generar umeros desordenados sin repetir
+	
+}
+
+
+int main(){
+int wh1 = 0;
+	
+	
+cout<<"___________________________________________________"<<endl;
+cout<<"________________Carrera  del  orden________________"<<endl;
+Sleep(1000);
+cout<<"      deberas elegir con cual de ls 4 opciones       "<<endl;
+cout<<"  crees que se ordenara mas rapido los numeros y de la forma en la que se entregaran    \n     para ser de los mejores guardianes del reino"<<endl;
+cout<<"___________________________________________________"<<endl;
+Sleep(1000);
+cout<<"."<<endl;
+Sleep(1000);
+cout<<"."<<endl;
+Sleep(1000);
+cout<<"."<<endl;
+Sleep(1000);
+cout<<"."<<endl;
+Sleep(1000);
+cout<<"."<<endl;
+Sleep(1000);
+system("cls");
+
+
+int question = 0;
+while(wh1== 0){
+	wh1 = 0;
+	cout<<"______________Que opcion desea elegir______________"<<endl;
+	cout<<"           Opcion________ordenado_________1             "<<endl;  //acas edaran las opciones de que deben elegirr suceptible a cambio
+	cout<<"           Opcion______ desordenado ______2             "<<endl;
+	cout<<"           Opcion________invertido________3             "<<endl;
+	cout<<"           Opcion_desordenado sin repetir_4             "<<endl;
+	cout<<"           Opcion___________salir_________5             "<<endl;
+	cout<<"___________________________________________________"<<std::endl;
+	cin>> question;
+	switch(question){
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+				
+	
+			break;
+		case 4:
+			
+			break;
+		case 5:	
+			wh1= 1;
+			break;
+		default: cout<<"incorrecto vuelva a ingresar"<<endl;
+	}
+}
+	
+	
+	
+	
+	return 0;
+}
